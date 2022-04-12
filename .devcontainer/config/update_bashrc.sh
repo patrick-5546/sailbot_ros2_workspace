5e353f863a000638f2fb7e89940d626672166285
Patrick Creighton <pcreighton429@gmail.com>
2022-04-12 01:48:52 +0000

    Restructure docker images

--- .devcontainer/config/update_bashrc.sh
+++ .devcontainer/config/update_bashrc.sh
@@ -1,7 +1,14 @@
 # Adding all the necessary ros sourcing
+echo "export DIPLAY=:0" >> ~/.bashrc
 echo "" >> ~/.bashrc
 echo "## ROS" >> ~/.bashrc
 echo "source /usr/share/colcon_cd/function/colcon_cd.sh" >> ~/.bashrc
 echo "export _colcon_cd_root=/workspaces/sailbot_ros2_workspace" >> ~/.bashrc
-# echo "source /workspaces/sailbot_ros2_workspace/install/setup.bash" >> ~/.bashrc
-# echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "# unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
+echo "unset ROS_DISTRO" >> ~/.bashrc
+echo "# setup ros1 environment" >> ~/.bashrc
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "# unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
+echo "unset ROS_DISTRO" >> ~/.bashrc
+echo "# setup ros2 environment" >> ~/.bashrc
+echo "source /workspaces/sailbot_ros2_workspace/install/setup.bash" >> ~/.bashrc
