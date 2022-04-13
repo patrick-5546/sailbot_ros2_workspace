465ba068942636fa266413e3a8b28b578c6fb133
Patrick Creighton <pcreighton429@gmail.com>
2022-04-12 21:27:56 -0700

    Finalize docker images

--- .devcontainer/dev/update_bashrc.sh
+++ .devcontainer/dev/update_bashrc.sh
@@ -6,7 +6,7 @@ echo "export _colcon_cd_root=/workspaces/sailbot_ros2_workspace" >> ~/.bashrc
 echo "# unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
 echo "unset ROS_DISTRO" >> ~/.bashrc
 echo "# setup ros1 environment" >> ~/.bashrc
-echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "source /workspaces/catkin_ws/devel/setup.bash" >> ~/.bashrc
 echo "# unsetting ROS_DISTRO to silence ROS_DISTRO override warning" >> ~/.bashrc
 echo "unset ROS_DISTRO" >> ~/.bashrc
 echo "# setup ros2 environment" >> ~/.bashrc
